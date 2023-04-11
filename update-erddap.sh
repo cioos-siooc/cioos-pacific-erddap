b58ebf8e8afa5339c2d2567bc2f4b23346647416
Your Name <you@example.com>
2023-04-11 17:33:06 +0000

    add docs and arguments to update-erdadp.sh

--- update-erddap.sh
+++ update-erddap.sh
@@ -1,16 +1,53 @@
 #!/bin/bash
 
-# SHA=$(git rev-parse HEAD)
+# PARAMETERS
 DATASET_XMLS_DIRS='datasets.d/'
+FLAG_DIR='erddap/data/flag/'
 HARD_FLAG_DIR='erddap/data/hardFlag/'
 CONTAINER_NAME=${CONTAINER_NAME:-erddap}
 
-# Define SHA to review from
-if [ -z $1 ]; then 
+# Documentation
+Help()
+{
+   # Display Help
+   echo "update-erddap.sh review any changes made between two commits and generate ERDDAP datasets flags for each associated datasets"
+   echo
+   echo "Syntax: update-erddap.sh [--sha *, --hardFlag, -h|--help]"
+   echo "options:"
+   echo "hardFlag  Generate Hard Flag. default flag"
+   echo "sha       Git SHA to review change from. If not given use sha available in .last_update_sha or present git SHA."
+   echo "help (h)  Print this Help."
+   echo
+}
+
+# Argument Parsing
+VALID_ARGS=$(getopt -o h --long sha:,hardFlag,help -- "$@")
+if [[ $? -ne 0 ]]; then
+    exit 1;
+fi
+
+# Parse arguments
+ADD_FLAG_DIR=$FLAG_DIR
+eval set -- "$VALID_ARGS"
+while [ : ]; do
+   case "$1" in
+      -h | --help) Help;exit;;
+      --sha) SHA=$2; shift 2;;
+      --hardFlag) ADD_FLAG_DIR=$HARD_FLAG_DIR; shift;;
+      \?) # Invalid option
+         echo "Error: Invalid option -> $option"
+         exit;;
+      -- ) shift; break ;;
+   esac
+done
+
+# Define SHA to review from if not provided
+if [ -z ${SHA} ] && [ -f .last_update_sha ]; then 
+echo 'update based on .last_update_sha file'
+SHA=$(cat .last_update_sha)
+elif [ -z ${SHA} ]; then
+echo 'Update based on present SHA'
 SHA=$(git rev-parse HEAD)
-else
-echo review specific sha $1
-SHA=$1
 fi
 
 git pull
@@ -36,3 +73,6 @@ do
     echo add hardFlag $file                                                                       
     touch "$HARD_FLAG_DIR$file"
 done
+
+# Save NEW_SHA to .last_update_sha
+echo $NEW_SHA > .last_update_sha
