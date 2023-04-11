575dfd15cb25dcc591da900bf920e3ffee91975d
Jessy Barrette <30420025+JessyBarrette@users.noreply.github.com>
2023-04-11 10:44:01 -0400

    add ERDDAP_DATSETS_DIR and ERDDAP_CONTAINER_NAME to DasDds and GenerateDatasetsXml

--- GenerateDatasetsXml.sh
+++ GenerateDatasetsXml.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+[ -f ./.env ] && source ./.env
 docker run --rm -it \
-  -v "$(pwd)/datasets:/datasets" \
-  -v "$(pwd)/logs:/erddapData/logs" \
-  axiom/docker-erddap:latest \
+  -v "${ERDDAP_DATASETS_DIR:-$(pwd)/datasets}:/datasets" \
+  -v "$(pwd)/erddap/data/logs:/erddapData/logs" \
+  ${ERDDAP_CONTAINAR_NAME:-axiom/docker-erddap:latest} \
   bash -c "cd webapps/erddap/WEB-INF/ && bash GenerateDatasetsXml.sh -verbose"
