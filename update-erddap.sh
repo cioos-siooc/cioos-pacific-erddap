da4265695c5c5502ab92e198297d6158d71d172f
Jessy Barrette <30420025+JessyBarrette@users.noreply.github.com>
2024-01-10 11:22:51 -0500

    Bring changes from dev to main branch

--- update-erddap.sh
+++ update-erddap.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 # PARAMETERS
+APPLY_HARD_FLAG_DIR=false
+MODE='pull'
 DATASET_XMLS_DIRS='datasets.d/'
-FLAG_DIR='erddap/data/flag/'
 HARD_FLAG_DIR='erddap/data/hardFlag/'
 CONTAINER_NAME=${CONTAINER_NAME:-erddap}
 
@@ -14,37 +15,54 @@ Help()
    echo
    echo "Syntax: update-erddap.sh [--sh *, --hardFlag, -h|--help, --mode|-m]"
    echo "options:"
-   echo "  mode      (all|pull|commited) Method used to determinate which commits to compare:"
+   echo "  --mode      (all|pull|commited) Method used to determinate which commits to compare:"
    echo "       - all: any active changes including none committed ones since the last update."
    echo "       - pull (default): run git pull and get changes since the last update"
    echo "       - no-pull: no git pull and get changes since the last update"
-   echo "  hardFlag  Generate Hard Flag. Default to flag"
-   echo "  sha       Git SHA to review changes from. If not given use sha available in .last_update_sha or present git SHA."
-   echo "  help,h    Print this Help."
+   echo "  --hardFlag  Generate Hard Flag"
+   echo "  --sha       Git SHA to review changes from. If not given use sha available in .last_update_sha or present git SHA."
+   echo "  --help,-h   Print this Help."
    echo
 }
 
 # Argument Parsing
-VALID_ARGS=$(getopt -o hm --long sha:,hardFlag,help,mode: -- "$@")
-if [[ $? -ne 0 ]]; then
-    exit 1;
-fi
-
-# Parse arguments
-ADD_FLAG_DIR=$FLAG_DIR
-MODE='pull'
+VALID_ARGS=$(getopt -o hm --long sha:,hardFlag,help,mode:,dasDds -- "$@")
+[ $? -eq 0 ] || { 
+    echo "Incorrect options provided"
+    exit 1
+}
+echo VALID_ARGS=$VALID_ARGS
 eval set -- "$VALID_ARGS"
-while [ : ]; do
-   case "$1" in
-      -h | --help) Help;exit;;
-      --sha) SHA=$2; shift 2;;
-      --hardFlag) ADD_FLAG_DIR=$HARD_FLAG_DIR; shift;;
-      \?) # Invalid option
-         echo "Error: Invalid option -> $option"
-         exit;;
-      -m | --mode) MODE=$2; echo "run mode=$MODE"; shift 2;;
-      -- ) shift; break ;;
-   esac
+while true; do
+    case "$1" in
+    -h|--help)
+        Help;exit
+        ;;
+    --sha)
+        SHA=$2;
+        echo Compare to SHA=$SHA
+        shift;
+        ;;
+    --hardFlag)
+        APPLY_HARD_FLAG_DIR=true
+        ;;
+    -m|--mode)
+        MODE=$2;
+        [[ ! $COLOR =~ all|pull|commited ]] && {
+            echo "Incorrect options provided"
+            exit 1
+        }
+        shift;
+        ;;
+    \?)
+        echo "Error: Invalid option -> $VALID_ARGS";
+        exit;; 
+    --)
+        shift
+        break
+        ;;
+    esac
+    shift
 done
 
 # Define SHA to review from if not provided
@@ -81,14 +99,15 @@ sudo docker exec $CONTAINER_NAME bash /init.d/replace-datasets-secrets.sh
 
 # Get xml files that were modified[M] or added[A] since the last commit  and generate flag
 DATASETS_IDS=$(git diff --diff-filter=AMCR --name-only --relative=$DATASET_XMLS_DIRS $SHA $NEW_SHA $DATASET_XMLS_DIRS | sed 's:.xml::')
-
 echo modified $DATASETS_IDS
 # generate a flag for each related datasetID
-for file in $DATASETS_IDS 
+if $APPLY_HARD_FLAG_DIR; then
+for DATASETS_ID in $DATASETS_IDS 
 do
-    echo add hardFlag $file                                                                       
-    touch "$HARD_FLAG_DIR$file"
+    echo add hardFlag $DATASETS_ID                                                                       
+    sudo touch "$HARD_FLAG_DIR$DATASETS_ID"
 done
+fi
 
 # Save NEW_SHA to .last_update_sha
 if [ "$NEW_SHA" != '' ]; then
